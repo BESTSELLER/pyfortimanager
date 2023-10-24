681ca9f61957214f8152c17581aa9817d2564ca3
Rasmus Thing <rasmus.thing@bestseller.com>
2023-10-24 21:50:23 +0200

    Add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyfortimanager",
     version="1.0.0",
     description="Python API client library for Fortinet's FortiManager.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="Apache2",
     author="Rasmus Thing",
     author_email="rasmus.thing@bestseller.com",
