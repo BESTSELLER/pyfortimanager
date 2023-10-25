2fdf5c302a345c03d1d858bdf289373968519e4c
Rasmus Thing <rasmus.thing@bestseller.com>
2023-10-25 21:21:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyfortimanager",
-    version="1.0.1",
+    version="1.0.2",
     description="Python API client library for Fortinet's FortiManager.",
     long_description=long_description,
     long_description_content_type="text/markdown",
