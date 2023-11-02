1f0a432fa01e51a510a3b3df2420f305ac11fad6
Rasmus Thing <rasmus.thing@bestseller.com>
2023-11-02 07:49:52 +0100

    Added vdom and switch_id filter

--- pyfortimanager/models/fortiswitches_proxy.py
+++ pyfortimanager/models/fortiswitches_proxy.py
@@ -8,11 +8,13 @@ class FortiSwitches_Proxy(FortiManager):
     def __init__(self, **kwargs):
         super(FortiSwitches_Proxy, self).__init__(**kwargs)
 
-    def all(self, fortigate: str, adom: str=None, timeout: int=None):
+    def all(self, fortigate: str, vdom: str="root", switch_id: str=None, adom: str=None, timeout: int=None):
         """Retrieves a list of all managed FortiSwitches on the FortiGate.
 
         Args:
             fortigate (str): Name of the FortiGate.
+            vdom (str): Name of the virtual domain for the FortiGate.
+            switch_id (str, optional): Serial number of a specific FortiSwitch.
             adom (str): Name of the ADOM. Defaults to the ADOM set when the API was instantiated.
             timeout (int, optional): How long to wait for the FortiGate to respond. Defaults to the proxy_timeout set when the API was instantiated.
 
@@ -28,9 +30,13 @@ class FortiSwitches_Proxy(FortiManager):
                         f"/adom/{adom or self.api.adom}/device/{fortigate}"
                     ],
                     "action": "get",
-                    "resource": "/api/v2/monitor/switch-controller/managed-switch/status"
+                    "resource": f"/api/v2/monitor/switch-controller/managed-switch/status?vdom={vdom}"
                 }
         }
+
+        # Optional fields
+        if switch_id:
+            params['data']['resource'] += f"&mkey={switch_id}"
 
         return self.post(method="exec", params=params)
 
