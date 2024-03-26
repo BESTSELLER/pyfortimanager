a7bf6ce0543131f23d900f70ff89b5e3051d05e2
RasmusThing <rasmus.thing@bestseller.com>
2024-03-26 09:28:57 +0100

    added adom and fortigate for add, update, delete

--- pyfortimanager/models/fortiaps.py
+++ pyfortimanager/models/fortiaps.py
@@ -118,13 +118,13 @@ class FortiAPs(FortiManager):
 
         return self.post(method="get", params=params)
 
-    def add(self, name: str, wtp_id: str, fortigate: str, vdom: str = "root", wtp_profile: str = None, prefer_img_ver: str = None, description: str = None, adom: str = None):
-        """Adds a new FortiAP.
+    def add_to_adom(self, name: str, wtp_id: str, fortigate: str, vdom: str = "root", wtp_profile: str = None, prefer_img_ver: str = None, description: str = None, adom: str = None):
+        """Adds a new FortiAP as a model device in the FortiAP Manager (ADOM).
 
         Args:
             name (str): Name of the FortiAP.
             wtp_id (str): Serial number of the FortiAP.
-            fortigate (str): Name of the FortiGate connected to the FortiSwitch.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
             vdom (str): Name of the virtual domain for the FortiGate.
             wtp_profile (str, optional): Name of the AP profile used for this AP.
             prefer_img_ver (str, optional): Enforce the firmware version of the FortiAP. Ex. 7.2.3-b0365.
@@ -140,7 +140,7 @@ class FortiAPs(FortiManager):
             "data": {
                 "name": name,
                 "wtp-id": wtp_id,
-                "_is-model": 1
+                "_is-model": 1,
             },
             "scope member": [
                 {
@@ -162,12 +162,48 @@ class FortiAPs(FortiManager):
 
         return self.post(method="add", params=params)
 
-    def update(self, wtp_id: str, fortigate: str, vdom: str = "root", name: str = None, wtp_profile: str = None, description: str = None, adom: str = None):
-        """Updates a FortiAP.
+    def add_to_fortigate(self, name: str, wtp_id: str, fortigate: str, vdom: str = "root", wtp_profile: str = None, prefer_img_ver: str = None, description: str = None):
+        """Adds a new FortiAP as a model device on the FortiGate in the FortiAP Manager.
+
+        Args:
+            name (str): Name of the FortiAP.
+            wtp_id (str): Serial number of the FortiAP.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
+            vdom (str): Name of the virtual domain for the FortiGate.
+            wtp_profile (str, optional): Name of the AP profile used for this AP.
+            prefer_img_ver (str, optional): Enforce the firmware version of the FortiAP. Ex. 7.2.3-b0365.
+            description (str, optional): Description of the FortiAP.
+
+        Returns:
+            dict: JSON data.
+        """
+
+        params = {
+            "url": f"/pm/config/device/{fortigate}/vdom/{vdom}/wireless-controller/wtp",
+            "data": {
+                "name": name,
+                "wtp-id": wtp_id
+            }
+        }
+
+        # Optional fields
+        if wtp_profile:
+            params['data']['wtp-profile'] = wtp_profile
+
+        if prefer_img_ver:
+            params['data']['_prefer-img-ver'] = prefer_img_ver
+
+        if description:
+            params['data']['location'] = description
+
+        return self.post(method="add", params=params)
+
+    def update_in_adom(self, wtp_id: str, fortigate: str, vdom: str = "root", name: str = None, wtp_profile: str = None, description: str = None, adom: str = None):
+        """Updates a FortiAP in the FortiAP Manager (ADOM).
 
         Args:
             wtp_id (str): Serial number of the FortiAP to update.
-            fortigate (str): Name of the FortiGate connected to the FortiSwitch.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
             vdom (str): Name of the virtual domain for the FortiGate.
             name (str, optional): Name of the FortiAP.
             wtp_profile (str, optional): Name of the AP profile used for this AP.
@@ -179,7 +215,7 @@ class FortiAPs(FortiManager):
         """
 
         params = {
-            "url": f"/pm/config/adom/{adom or self.api.adom}/obj/wireless-controller/wtp",
+            "url": f"/pm/config/adom/{adom or self.api.adom}/obj/wireless-controller/wtp/{wtp_id}",
             "data": {
                 "wtp-id": wtp_id
             },
@@ -203,12 +239,51 @@ class FortiAPs(FortiManager):
 
         return self.post(method="update", params=params)
 
-    def delete(self, wtp_id: str, fortigate: str, vdom: str = "root", adom: str = None):
-        """Deletes a FortiAP.
+    def update_on_fortigate(self, wtp_id: str, fortigate: str, vdom: str = "root", name: str = None, wtp_profile: str = None, description: str = None):
+        """Updates a FortiAP on the FortiGate in the FortiAP Manager.
+
+        Args:
+            wtp_id (str): Serial number of the FortiAP to update.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
+            vdom (str): Name of the virtual domain for the FortiGate.
+            name (str, optional): Name of the FortiAP.
+            wtp_profile (str, optional): Name of the AP profile used for this AP.
+            description (str, optional): Description of the FortiAP.
+
+        Returns:
+            dict: JSON data.
+        """
+
+        params = {
+            "url": f"/pm/config/device/{fortigate}/vdom/{vdom}/wireless-controller/wtp",
+            "data": {
+                "wtp-id": wtp_id
+            },
+            "filter": [
+                "wtp-id",
+                "==",
+                wtp_id
+            ]
+        }
+
+        # Optional fields
+        if name:
+            params['data']['name'] = name
+
+        if wtp_profile:
+            params['data']['wtp-profile'] = wtp_profile
+
+        if description:
+            params['data']['location'] = description
+
+        return self.post(method="update", params=params)
+
+    def delete_in_adom(self, wtp_id: str, fortigate: str, vdom: str = "root", adom: str = None):
+        """Deletes a FortiAP in the FortiAP Manager (ADOM).
 
         Args:
             wtp_id (str): Serial number of the FortiAP to delete.
-            fortigate (str): Name of the FortiGate connected to the FortiSwitch.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
             vdom (str): Name of the virtual domain for the FortiGate.
             adom (str): Name of the ADOM. Defaults to the ADOM set when the API was instantiated.
 
@@ -223,7 +298,25 @@ class FortiAPs(FortiManager):
                     "name": fortigate,
                     "vdom": vdom
                 }
-            ]
+            ],
+        }
+
+        return self.post(method="delete", params=params)
+
+    def delete_on_fortigate(self, wtp_id: str, fortigate: str, vdom: str = "root"):
+        """Deletes a FortiAP on the FortiGate in the FortiAP Manager.
+
+        Args:
+            wtp_id (str): Serial number of the FortiAP to delete.
+            fortigate (str): Name of the FortiGate connected to the FortiAP.
+            vdom (str): Name of the virtual domain for the FortiGate.
+
+        Returns:
+            dict: JSON data.
+        """
+
+        params = {
+            "url": f"/pm/config/device/{fortigate}/vdom/{vdom}/wireless-controller/wtp/{wtp_id}",
         }
 
         return self.post(method="delete", params=params)
